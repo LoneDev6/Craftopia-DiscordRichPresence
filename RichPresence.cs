9ae5dd6dcbf1c37f18c512e58129f9c5eacdd1ce
LoneDev <leoxy98@gmail.com>
2020-09-28 13:40:39 +0200

    Update RichPresence.cs

--- RichPresence.cs
+++ RichPresence.cs
@@ -2,6 +2,7 @@ using BepInEx;
 using Oc;
 using System;
 using UnityEngine.SceneManagement;
+using UniRx;
 
 namespace CraftopiaDiscord
 {
@@ -10,11 +11,9 @@ namespace CraftopiaDiscord
     public class RichPresence : BaseUnityPlugin
     {
         long CLIENT_ID = 759792773087363094;
-
         Discord.Discord discord;
 
         long startTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-        int playersCount = 0;
 
         void Awake()
         {
@@ -22,20 +21,44 @@ namespace CraftopiaDiscord
 
             discord = new Discord.Discord(CLIENT_ID, (UInt64)Discord.CreateFlags.NoRequireDiscord);
 
+            RefreshAndSendActivity();
             SceneManager.activeSceneChanged += SceneManager_activeSceneChanged;
         }
 
-        private void SceneManager_activeSceneChanged(Scene arg0, Scene arg1)
+        void Update()
         {
-            UnityEngine.Debug.Log("------ Changed scene: " + SceneManager.GetActiveScene().name);
+            discord.RunCallbacks();
+        }
 
-            playersCount = FindObjectsOfType<OcCharacter>().Length;
+        private void SceneManager_activeSceneChanged(Scene oldScene, Scene newScene)
+        {
+            UnityEngine.Debug.Log("--- Changed scene: " + newScene.name);
 
-            var activityManager = discord.GetActivityManager();
-            var activity = new Discord.Activity
+            RefreshAndSendActivity();
+
+            OcPl player = SingletonMonoBehaviour<OcNetMng>.FindObjectOfType<OcPl>();
+            if (player != null)
             {
-                Details = GetDetailsBySceneName(arg1.name),
-                State = playersCount > 1 ? "Multiplayer" : "Singleplayer",
+                UnityEngine.Debug.Log("------ Registered AddExpEvent");
+                OcPlLevelCtrl levelCtrl = player.PlLevelCtrl;
+                levelCtrl.AddExpEvent += LevelCtrl_AddExpEvent;
+            }
+        }
+
+        private void LevelCtrl_AddExpEvent(long exp)
+        {
+            UnityEngine.Debug.Log($"AddExpEvent +{exp}");
+            RefreshAndSendActivity();
+        }
+
+
+        private Discord.Activity BuildActivity()
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            return new Discord.Activity
+            {
+                Details = GetGameDetailsBySceneName(currentSceneName), //top
+                State = GetGameStateBySceneName(currentSceneName), //bottom
                 Assets = new Discord.ActivityAssets
                 {
                     LargeImage = "main"
@@ -45,22 +68,17 @@ namespace CraftopiaDiscord
                     Start = startTime
                 }
             };
-            activityManager.UpdateActivity(activity, (res) =>
-            {
+        }
+
+        private void RefreshAndSendActivity()
+        {
+            discord.GetActivityManager().UpdateActivity(BuildActivity(), (res) => {
                 if (res != Discord.Result.Ok)
-                {
                     UnityEngine.Debug.LogError("Error setting Discord Presence: " + res.ToString());
-                }
             });
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            discord.RunCallbacks();
-        }
-
-        string GetDetailsBySceneName(string scene)
+        string GetGameDetailsBySceneName(string scene)
         {
             switch(scene)
             {
@@ -69,11 +87,25 @@ namespace CraftopiaDiscord
                 case "OcScene_Setting":
                     return "Loading...";
 
-                case "OcScene_DevTest_yohei_Tutorial_0728_MS":
-                    return "Ingame";
+                //case "OcScene_DevTest_yohei_Tutorial_0728_MS": //first island scene
+                //    return null;
 
                 default:
-                    return "Ingame";
+                    return $"Level {NetWrapper.PlayerLevel} ({NetWrapper.PlayerExp}/{NetWrapper.PlayerNextLevelExp}xp)";
+            }
+        }
+
+        private string GetGameStateBySceneName(string scene)
+        {
+            switch (scene)
+            {
+                case "OcScene_Home":
+                case "OcScene_Setting":
+                    return "";
+
+                //case "OcScene_DevTest_yohei_Tutorial_0728_MS": //first island scene
+                default:
+                    return $"{(NetWrapper.IsMultiplayer ? $"Multiplayer ({NetWrapper.ConnectedPlayersCount} of {NetWrapper.MaxPlayers})" : "Singleplayer")} ";
             }
         }
     }
